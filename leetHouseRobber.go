f72467754e85f05d3912ecb37da88deccd292d1a
Ruslan Gusmanov <gusmanovruslan@gmail.com>
2023-12-23 21:38:08 +0300

    General approach for a Fibonacci style problem

--- leetHouseRobber.go
+++ leetHouseRobber.go
@@ -2,16 +2,11 @@ package leetHouseRobber
 
 func rob(nums []int) int {
 	n := len(nums)
-	dp := make([]int, n)
-	var i int
-	for i = 0; i == 0 && i < n; i++ {
-		dp[i] = nums[i]
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = nums[0]
+	for i := 2; i <= n; i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i-1])
 	}
-	for i = 1; i == 1 && i < n; i++ {
-		dp[i] = max(nums[i], nums[i-1])
-	}
-	for i = 2; i < n; i++ {
-		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
-	}
-	return dp[n-1]
+	return dp[n]
 }
